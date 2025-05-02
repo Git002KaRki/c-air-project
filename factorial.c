28ff2c7a3356b34c42d7464301a89699e21c93eb
Git002KaRki <karkimithila480@gmail.com>
2025-05-02 08:29:34 +0545

    file update

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    int main()
+     {
+        int num;
+        int fact=1;
+        printf("Enter a number:");
+        scanf("%d",&num);
+        for(int i=2;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        printf("factorial is %d",fact);
+        return 0;
+    }
+    
+
+
+
+
